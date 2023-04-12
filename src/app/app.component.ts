379a76a62e8462383e7299090d46daafa562066b
Sanprieto <ricardosanprieto@gmail.com>
2023-04-12 17:09:23 +0200

    @Input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-components';
+  imgParent = '';
+  // https://www.w3schools.com/howto/img_avatar.png
 }
---
+++ src/app/components/img/img.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input} from '@angular/core';
+
+@Component({
+  selector: 'app-img',
+  templateUrl: './img.component.html',
+  styleUrls: ['./img.component.scss']
+})
+export class ImgComponent {
+
+@Input() img: string = 'valor init'
+
+}
